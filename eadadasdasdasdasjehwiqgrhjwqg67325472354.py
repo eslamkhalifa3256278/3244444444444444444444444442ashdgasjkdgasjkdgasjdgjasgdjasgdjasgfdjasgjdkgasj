eee6d6cd03d50fca22e4c7573466d49ee4db6076
eslamkhalifa3256278 <aaaeaaasaaalaaaaaaam@gmail.com>
2025-04-30 01:57:49 +0300

    Update eadadasdasdasdasjehwiqgrhjwqg67325472354.py

--- eadadasdasdasdasjehwiqgrhjwqg67325472354.py
+++ eadadasdasdasdasjehwiqgrhjwqg67325472354.py
@@ -37,7 +37,7 @@ def app():
     def create_account():
         col1, col2 = st.columns([1, 2])
         with col1:
-            st.image(LOGO_URL, width=300)
+            st.image(LOGO_URL, width=500)
         
         with col2:
             st.markdown("""
